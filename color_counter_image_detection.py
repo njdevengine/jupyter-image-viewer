330d35991494941d1c106a9922e853091829e59a
njdevengine <newjerseydevelopment@gmail.com>
2019-06-12 15:24:22 -0400

    Create color_counter_image_detection.py

---
+++ color_counter_image_detection.py
@@ -0,0 +1,24 @@
+import os
+from os import listdir
+files = os.listdir('/ml/')
+
+dictionary = {}
+from PIL import Image
+n=0
+for i in files:
+    number = int(i[7:].split(".")[0])
+    colors = len(Image.open("ml/"+i,mode="r").getcolors(maxcolors=9999999))
+    dictionary[number] = colors
+    print(n)
+    n+=1
+
+#identifies default avatars for twitter profile images(they use 184 colors)
+
+from IPython.display import Image, display
+n=0
+for i in dictionary:
+    if dictionary[i] == 184:
+        print(dictionary[i])
+        display(Image(filename='/ml/outfile'+str(i)+'.png'))
+        n+=1
+print(n)
