abdb0878b94cfc06e3ce0bf364046d606acf78d0
njdevengine <newjerseydevelopment@gmail.com>
2019-06-13 09:18:03 -0400

    Update dl_img_count_update_df.py

--- dl_img_count_update_df.py
+++ dl_img_count_update_df.py
@@ -3,8 +3,6 @@ import urllib.request
 from PIL import Image as img
 import pandas as pd
 
-#download image from url, count colors, update dataframe
-
 df = pd.read_excel('twitter_followers_detailed.xlsx')
 print('got df')
 n = 0
@@ -12,9 +10,9 @@ for imageName in list(df["profile_image_url"]):
     try:
         urllib.request.urlretrieve(imageName, '/ml/out.png')
         colors = len(img.open("ml/out.png",mode="r").getcolors(maxcolors=9999999))
-        df["color_number"] = colors
+        df.at[n,"color_number"] = colors    
     except:
-        df["color_number"] = "image_not_found"
+        df.at[n,"color_number"] = 0.01
     n+=1
     print(n)
 df.to_csv('update.csv')
